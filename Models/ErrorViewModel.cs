289b0afbcd176382d946542431bddc449b64e89c
motst <motst@DESKTOP-U1MOIF4>
2024-10-30 02:38:22 -0500

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Fall2024_Assignment3_mtorres3.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
