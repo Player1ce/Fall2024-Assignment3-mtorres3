88e0c230e6537d954ee54a25c7e167decb8b7e5c
Marcelo Torres <mtorres3@crimson.ua.edu>
2024-10-31 02:03:49 -0500

    Updated system to use Azure database and tested in deployment environment. It works online!(:

--- Program.cs
+++ Program.cs
@@ -5,12 +5,21 @@ using Microsoft.EntityFrameworkCore;
 using System.Configuration;
 using Azure;
 using Azure.AI.OpenAI;
+using Microsoft.Data.SqlClient;
 using OpenAI.Chat;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+//var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
+string configConnectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+                                ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
+var conStrBuilder = new SqlConnectionStringBuilder(configConnectionString);
+conStrBuilder.Password = builder.Configuration["DB_PASSWORD"];
+string connectionString = conStrBuilder.ConnectionString;
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
@@ -19,29 +28,14 @@ builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.Requ
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
 
-//var apiKey = builder.Configuration["AZURE_OPENAI_KEY"];
-//var endpoint = builder.Configuration["AZURE_OPENAI_ENDPOINT"];
-//var deploymentName = builder.Configuration["DEPLOYMENT_NAME"];
 
-//if (string.IsNullOrEmpty(apiKey))
-//{
-//    Console.WriteLine("ApiKey is null or empty");
-//}
 
-//Console.WriteLine($"ApiKey: {apiKey}");
-//Console.WriteLine($"endpoint: {endpoint}");
-//Console.WriteLine($"deploymentName: {deploymentName}");
 
 builder.Services.AddSingleton<ChatClient>(sp =>
 {
     var apiKey = builder.Configuration["AZURE_OPENAI_KEY"];
     var endpoint = builder.Configuration["AZURE_OPENAI_ENDPOINT"];
     var deploymentName = builder.Configuration["DEPLOYMENT_NAME"];
-    
-    //Console.WriteLine("In Singleton Service.");
-    //Console.WriteLine($"ApiKey: {apiKey}");
-    //Console.WriteLine($"endpoint: {endpoint}");
-    //Console.WriteLine($"deploymentName: {deploymentName}");
 
     var azureClient = new AzureOpenAIClient(new Uri(endpoint), new AzureKeyCredential(apiKey));
     return azureClient.GetChatClient(deploymentName);
